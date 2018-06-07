ec6a59bc1e998d01f0f2cf9cd1a8b719095d5da2
Marcin Bilski <gyamtso@gmail.com>
2018-06-07 21:55:18 +0200

    Clean up + add missing spec.

--- spec/akasha/event_router_base_spec.rb
+++ spec/akasha/event_router_base_spec.rb
@@ -29,21 +29,21 @@ describe Akasha::EventRouterBase do
     context 'with multiple valid listeners' do
       before do
         router.register_event_listener(:name_changed, Notifier)
-        router.register_event_listener(:name_changed, Logger)
+        router.register_event_listener(:name_changed, ItemLogger)
         allow(router).to receive(:log).and_return(nil) # Do not print to stdout.
       end
 
       it 'invokes all event handlers' do
         expect_any_instance_of(Notifier).to receive(:on_name_changed).with('item-1', new_name: 'new name')
-        expect_any_instance_of(Logger).to receive(:on_name_changed).with('item-1', new_name: 'new name')
+        expect_any_instance_of(ItemLogger).to receive(:on_name_changed).with('item-1', new_name: 'new name')
         expect { subject }.to_not raise_error
       end
 
       it 'ignores exceptions' do
         expect_any_instance_of(Notifier).to receive(:on_name_changed).with('item-1', new_name: 'new name')
                                                                      .and_raise('Oops!')
-        expect_any_instance_of(Logger).to receive(:on_name_changed).with('item-1', new_name: 'new name')
-                                                                   .and_raise('Oops!')
+        expect_any_instance_of(ItemLogger).to receive(:on_name_changed).with('item-1', new_name: 'new name')
+                                                                       .and_raise('Oops!')
         expect { subject }.to_not raise_error
       end
     end
--- spec/fixtures/item_logger.rb
+++ spec/fixtures/item_logger.rb
@@ -1,5 +1,5 @@
 # An example event listener.
-class Logger < Akasha::EventListener
+class ItemLogger < Akasha::EventListener
   # Handle the 'name_changed' event.
   # Specs will spy on this method.
   def on_name_changed(_aggregate_id, **)
