c5e419425a1fa6abcc8f1fc9f6fe9d39abcff764
Marcin Bilski <gyamtso@gmail.com>
2018-06-08 09:46:46 +0200

    Run integration tests in Travis.

--- bin/integration-tests.sh
+++ bin/integration-tests.sh
@@ -4,25 +4,32 @@
 RED='\033[0;31m'
 GREEN='\033[0;32m'
 NC='\033[0m'
+
+# project name has to be "docker" likely because Travis uses an older version
+# of docker-compose ignoring -p.
+PROJECT=docker
+
 # kill and remove any running containers
 cleanup () {
-  docker-compose -p ci kill
-  docker-compose -p ci rm -f --all
+  docker-compose -p $PROJECT kill
+  docker-compose -p $PROJECT rm -f --all
 }
 # catch unexpected failures, do cleanup and output an error message
 trap 'cleanup ; printf "${RED}Tests Failed For Unexpected Reasons${NC}\n"'\
   HUP INT QUIT PIPE TERM
 cd docker
 # build and run the composed services
-docker-compose -p ci build && docker-compose -p ci up -d
+docker-compose -p $PROJECT build && docker-compose -p $PROJECT up -d
 if [ $? -ne 0 ] ; then
   printf "${RED}Docker Compose Failed${NC}\n"
   exit -1
 fi
+echo "Waiting for tests to finish"
 # wait for the test service to complete and grab the exit code
-TEST_EXIT_CODE=`docker wait ci_tests_1`
+TEST_EXIT_CODE=`docker wait ${PROJECT}_tests_1`
+echo "Listing docker logs"
 # output the logs for the test (for clarity)
-docker logs ci_tests_1
+docker logs ${PROJECT}_tests_1
 # inspect the output of the test and display respective message
 if [ -z ${TEST_EXIT_CODE+x} ] || [ "$TEST_EXIT_CODE" -ne 0 ] ; then
   printf "${RED}Tests Failed${NC} - Exit Code: $TEST_EXIT_CODE\n"
--- spec/akasha/storage/http_event_store_spec.rb
+++ spec/akasha/storage/http_event_store_spec.rb
@@ -1,6 +1,3 @@
 describe "Akasha::Storage::HttpEventStore", integration: true do
-  it 'ee' do
-    expect(true).to be false
-  end
 
 end
