0763ad8347c1ebd545a554ca160937416bf1c063
Marcin Bilski <gyamtso@gmail.com>
2018-06-11 22:14:50 +0200

    Es-based checkpoints.

--- lib/akasha/storage/http_event_store/stream.rb
+++ lib/akasha/storage/http_event_store/stream.rb
@@ -3,15 +3,17 @@ module Akasha
     class HttpEventStore
       # HTTP Eventstore stream.
       class Stream
+        attr_reader :name
+
         def initialize(client, stream_name)
           @client = client
-          @stream_name = stream_name
+          @name = stream_name
         end
 
         # Appends events to the stream.
         def write_events(events)
           return if events.empty?
-          @client.retry_append_to_stream(@stream_name, events)
+          @client.retry_append_to_stream(@name, events)
         end
 
         # Reads events from the stream starting from `start` inclusive.
@@ -29,18 +31,18 @@ module Akasha
               position += events.size
             end
           else
-            @client.retry_read_events_forward(@stream_name, start, page_size, poll)
+            @client.retry_read_events_forward(@name, start, page_size, poll)
           end
         end
 
         # Reads stream metadata.
         def metadata
-          @client.read_metadata(@stream_name)
+          @client.retry_read_metadata(@name)
         end
 
         # Updates stream metadata.
         def metadata=(metadata)
-          @client.write_metadata(@stream_name, metadata)
+          @client.retry_write_metadata(@name, metadata)
         end
       end
     end
---
+++ spec/akasha/checkpoint/http_event_store_checkpoint_spec.rb
@@ -0,0 +1,98 @@
+describe Akasha::Checkpoint::HttpEventStoreCheckpoint, integration: true do
+  let(:checkpoint) { described_class.new(repository.streams[stream]) }
+  let(:stream) { "stream-#{SecureRandom.uuid}" }
+  let(:repository) { Akasha::Storage::HttpEventStore.new(http_es_config) }
+
+  let(:events) do
+    [
+      Akasha::Event.new(:name_changed, old_name: nil, new_name: 'new name'),
+      Akasha::Event.new(:name_changed, old_name: 'new_name', new_name: 'newest name')
+    ]
+  end
+
+  describe '#latest' do
+    subject { checkpoint.latest }
+
+    context 'for an empty stream' do
+      context 'for empty checkpoint' do
+        it { is_expected.to be_zero }
+      end
+    end
+
+    context 'for a non-empty stream' do
+      before do
+        repository.streams[stream].write_events(events)
+      end
+
+      context 'for empty checkpoint' do
+        it { is_expected.to be_zero }
+      end
+
+      context 'for checkpoint that was updated' do
+        before do
+          checkpoint.ack(0)
+          checkpoint.ack(1)
+          checkpoint.ack(4)
+        end
+
+        it { is_expected.to eq 5 }
+
+        it 'persists state' do
+          loaded = described_class.new(repository.streams[stream])
+          expect(loaded.latest).to eq 5
+        end
+      end
+    end
+  end
+
+  describe '#ack' do
+    subject { checkpoint.latest }
+    let(:other_instance) { described_class.new(repository[stream], interval: interval) }
+    let(:interval) { 1 }
+
+    context 'for an empty stream' do
+      it 'raises an error' do
+        expect { checkpoint.ack(0) }.to raise_error Akasha::Checkpoint::HttpEventStoreCheckpoint::StreamNotFoundError
+      end
+    end
+
+    context 'for a non-empty stream' do
+      before do
+        repository.streams[stream].write_events(events)
+      end
+
+      context 'with checkpoints after each event' do
+        let(:interval) { 1 }
+
+        (0..2).each do |position|
+          it "saves position #{position}" do
+            other_instance.ack(position)
+            expect(subject).to eq(position + 1)
+          end
+        end
+      end
+
+      context 'with checkpoint after every 3rd event' do
+        let(:interval) { 3 }
+
+        it 'does not save position 0' do
+          other_instance.ack(0)
+          expect(other_instance.latest).to eq 1
+          expect(subject).to eq(0)
+        end
+
+        it 'does not save position 1' do
+          other_instance.ack(1)
+          expect(other_instance.latest).to eq 2
+          expect(subject).to eq(0)
+        end
+
+        it 'saves position 2' do
+          other_instance.ack(2)
+          expect(other_instance.latest).to eq 3
+          expect(subject).to eq(3)
+        end
+      end
+    end
+  end
+end
